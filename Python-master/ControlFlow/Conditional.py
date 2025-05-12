37cf8eccac16ec5370496af67089afbc7ac01c85
Vikash Rathore <vikash.rathore@atyeti.com>
2025-05-12 15:11:44 +0530

    Conditional Statement

---
+++ Python-master/ControlFlow/Conditional.py
@@ -0,0 +1,15 @@
+# Age-Wise Distribution
+
+age = 25
+
+if age > 0:
+    if age <= 12:
+        print("Child")
+    elif age <= 19:
+        print("Teenager")
+    elif age <= 35:
+        print("Young Adult")
+    else:
+        print("Adult")
+else:
+    print("Invalid age")
