c23cce59e19ca399de0b8c19e016557a1810fc05
Vikash Rathore <vikash.rathore@atyeti.com>
2025-05-13 15:32:35 +0530

    ControlFlow

---
+++ Python-master/ControlFlow/loop_control.txt/break.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    if i == 5:
+        break  # Exit the loop when i is 5
+    print(i)
---
+++ Python-master/ControlFlow/loop_else_clause.py
@@ -0,0 +1,7 @@
+items = [12, 11, 15]
+for item in items:
+    if item == 14:
+        print("Found it!")
+        break
+else:
+    print("Item not found.")
